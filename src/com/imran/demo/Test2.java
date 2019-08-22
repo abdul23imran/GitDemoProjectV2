82e25207dd9bd399684fa61aab9ccc345b7ebabb
highfy life <abdul23imran@gmail.com>
2019-08-22 19:20:32 +0530

    first commit

---
+++ src/com/imran/demo/Test2.java
@@ -0,0 +1,18 @@
+package com.imran.demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+    String hungry="yes";
+    
+    if(hungry=="yes") {
+    	System.out.println("EAT SOMETHING..............");
+    }
+
+    else {
+    	System.out.println("DO YOUR WORK............");
+    }
+	}
+
+}
